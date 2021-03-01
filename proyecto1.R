96462e6d96111d7e9c578682a2345f47b6b1f4d2
ricardoVA999 <40800457+ricardoVA999@users.noreply.github.com>
2021-03-01 10:43:31 -0600

    Todos los datasets

--- proyecto1.R
+++ proyecto1.R
@@ -1,4 +1,4 @@
 library(haven)
-setwd("C:/Users/Zephyrus/Documents/U/7mo Semestre/Mineria de Datos/Proyecto1")
-dataset = read_sav("./BasesDeDatos/defunciones2009.sav")
-View(dataset)
+setwd("C:/Users/Zephyrus/Documents/U/7mo Semestre/Mineria de Datos/Proyecto1-Mineria-de-Datos")
+dataset = read_sav("./BasesDeDatos/nacimientos2010.sav")
+dataset2 = read_sav("./BasesDeDatos/nacimientos2009.sav")
