60455e961ddd1839c16b35785ef305e5097fc443
Sara Zavala <42972998+saritazavala@users.noreply.github.com>
2021-05-20 16:07:36 -0600

    Predictions updated

--- predicciones.R
+++ predicciones.R
@@ -9,6 +9,8 @@ library(rpart)
 library(rpart.plot)
 library(caret)
 
+
+
 #Procecamiento de datos para los datos de Nacimientos
 nac2009 = read_sav("./BasesDeDatos/nacimientos2009.sav")
 names(nac2009)[names(nac2009) == "Gretnm"] <- "grupetma"
@@ -20,8 +22,12 @@ nac2009$Naciop<-NULL
 nac2009$Naciom<-NULL
 nac2009$Ocupam<-NULL
 nac2009$Ocupap<-NULL
-nac2009$grupetma[nac2009$grupetma %in% 1] <- 6
-nac2009$grupetma[nac2009$grupetma %in% 2] <- 7
+nac2009$Gretnp[nac2009$Gretnp %in% 2] <- 4
+nac2009$grupetma[nac2009$grupetma %in% 2] <- 4
+nac2009$Asisrec[nac2009$Asisrec %in% 9] <- 5
+nac2009$Sitioocu[nac2009$Sitioocu %in% 4] <- 5
+nac2009$Sitioocu[nac2009$Sitioocu %in% 3] <- 4
+
 
 nac2010 = read_sav("./BasesDeDatos/nacimientos2010.sav")
 names(nac2010)[names(nac2010) == "mupnam"] <- "Mupnam"
@@ -33,12 +39,12 @@ nac2010$Naciop<-NULL
 nac2010$Naciom<-NULL
 nac2010$Ocupam<-NULL
 nac2010$Ocupap<-NULL
-nac2010$Gretnp[nac2010$Gretnp %in% 1] <- 6
-nac2010$Gretnp[nac2010$Gretnp %in% 2] <- 7
-nac2010$grupetma[nac2010$grupetma %in% 1] <- 6
-nac2010$grupetma[nac2010$grupetma %in% 2] <- 7
+nac2010$Gretnp[nac2010$Gretnp %in% 2] <- 4
+nac2010$grupetma[nac2010$grupetma %in% 2] <- 4
 nac2010$Escolam <- ifelse(nac2010$Escolam !=9,nac2010$Escolam+1,nac2010$Escolam )
 nac2010$Escolap <- ifelse(nac2010$Escolap !=9,nac2010$Escolap+1,nac2010$Escolap )
+nac2010$Sitioocu[nac2010$Sitioocu %in% 4] <- 5
+nac2010$Sitioocu[nac2010$Sitioocu %in% 3] <- 4
 
 nac2011 = read_sav("./BasesDeDatos/nacimientos2011.sav")
 names(nac2011)[names(nac2011) == "Mupreg"] <- "mupreg"
@@ -50,10 +56,8 @@ nac2011$Naciop<-NULL
 nac2011$Naciom<-NULL
 nac2011$Ocupam<-NULL
 nac2011$Ocupap<-NULL
-nac2011$Gretnp[nac2011$Gretnp %in% 1] <- 6
-nac2011$Gretnp[nac2011$Gretnp %in% 2] <- 7
-nac2011$grupetma[nac2011$grupetma %in% 1] <- 6
-nac2011$grupetma[nac2011$grupetma %in% 2] <- 7
+nac2011$Gretnp[nac2011$Gretnp %in% 2] <- 4
+nac2011$grupetma[nac2011$grupetma %in% 2] <- 4
 nac2011$Escolam <- ifelse(nac2011$Escolam !=9,nac2011$Escolam+1,nac2011$Escolam )
 nac2011$Escolap <- ifelse(nac2011$Escolap !=9,nac2011$Escolap+1,nac2011$Escolap )
 
@@ -69,10 +73,8 @@ nac2012$Naciop<-NULL
 nac2012$Naciom<-NULL
 nac2012$Ocupam<-NULL
 nac2012$Ocupap<-NULL
-nac2012$Gretnp[nac2012$Gretnp %in% 1] <- 6
-nac2012$Gretnp[nac2012$Gretnp %in% 2] <- 7
-nac2012$grupetma[nac2012$grupetma %in% 1] <- 6
-nac2012$grupetma[nac2012$grupetma %in% 2] <- 7
+nac2012$Gretnp[nac2012$Gretnp %in% 2] <- 4
+nac2012$grupetma[nac2012$grupetma %in% 2] <- 4
 nac2012$Escolam <- ifelse(nac2012$Escolam !=9,nac2012$Escolam+1,nac2012$Escolam )
 nac2012$Escolap <- ifelse(nac2012$Escolap !=9,nac2012$Escolap+1,nac2012$Escolap )
 
@@ -104,6 +106,9 @@ nac2014$Ocupap<-NULL
 nac2014$ciuomad<-NULL
 nac2014$Escolam <- ifelse(nac2014$Escolam !=9,nac2014$Escolam+1,nac2014$Escolam )
 nac2014$Escolap <- ifelse(nac2014$Escolap !=9,nac2014$Escolap+1,nac2014$Escolap )
+nac2014$Asisrec[nac2014$Asisrec %in% 4] <- 2
+nac2014$Asisrec[nac2014$Asisrec %in% 5] <- 4
+nac2014$Asisrec[nac2014$Asisrec %in% 6] <- 5
 
 nac2015 = read_sav("./BasesDeDatos/nacimientos2015.sav")
 names(nac2015)[names(nac2015) == "Mupreg"] <- "mupreg"
@@ -120,6 +125,9 @@ nac2015$Paisnacm<-NULL
 nac2015$ViaPar<-NULL
 nac2015$Ocupap<-NULL
 nac2015$Ocupam<-NULL
+nac2015$Asisrec[nac2015$Asisrec %in% 4] <- 2
+nac2015$Asisrec[nac2015$Asisrec %in% 5] <- 4
+nac2015$Asisrec[nac2015$Asisrec %in% 6] <- 5
 
 nac2016 = read_sav("./BasesDeDatos/nacimientos2016.sav")
 names(nac2016)[names(nac2016) == "Mupreg"] <- "mupreg"
@@ -138,6 +146,10 @@ nac2016$Ocupap<-NULL
 nac2016$Ocupam<-NULL
 nac2016$Escolam <- ifelse(nac2016$Escolam !=9,nac2016$Escolam+1,nac2016$Escolam )
 nac2016$Escolap <- ifelse(nac2016$Escolap !=9,nac2016$Escolap+1,nac2016$Escolap )
+nac2016$Asisrec[nac2016$Asisrec %in% 2] <- 3
+nac2016$Asisrec[nac2016$Asisrec %in% 4] <- 2
+nac2016$Asisrec[nac2016$Asisrec %in% 5] <- 4
+nac2016$Asisrec[nac2016$Asisrec %in% 6] <- 5
 
 nac2017 = read_sav("./BasesDeDatos/nacimientos2017.sav")
 names(nac2017)[names(nac2017) == "Mupreg"] <- "mupreg"
@@ -154,6 +166,10 @@ nac2017$Paisnacm<-NULL
 nac2017$ViaPar<-NULL
 nac2017$Ocupap<-NULL
 nac2017$Ocupam<-NULL
+nac2017$Asisrec[nac2017$Asisrec %in% 4] <- 2
+nac2017$Asisrec[nac2017$Asisrec %in% 3] <- 4
+nac2017$Asisrec[nac2017$Asisrec %in% 5] <- 3
+nac2017$Asisrec[nac2017$Asisrec %in% 6] <- 5
 
 nac2018 = read_sav("./BasesDeDatos/nacimientos2018.sav")
 names(nac2018)[names(nac2018) == "Mupreg"] <- "mupreg"
@@ -170,6 +186,10 @@ nac2018$Paisnacm<-NULL
 nac2018$ViaPar<-NULL
 nac2018$Ocupap<-NULL
 nac2018$Ocupam<-NULL
+nac2018$Asisrec[nac2018$Asisrec %in% 4] <- 2
+nac2018$Asisrec[nac2018$Asisrec %in% 3] <- 4
+nac2018$Asisrec[nac2018$Asisrec %in% 5] <- 3
+nac2018$Asisrec[nac2018$Asisrec %in% 6] <- 5
 
 nac2019 = read_sav("./BasesDeDatos/nacimientos2019.sav")
 names(nac2019)[names(nac2019) == "Mupreg"] <- "mupreg"
@@ -186,7 +206,10 @@ nac2019$Paisnacm<-NULL
 nac2019$ViaPar<-NULL
 nac2019$Ocupap<-NULL
 nac2019$Ocupam<-NULL
-
+nac2019$Asisrec[nac2019$Asisrec %in% 4] <- 2
+nac2019$Asisrec[nac2019$Asisrec %in% 3] <- 4
+nac2019$Asisrec[nac2019$Asisrec %in% 5] <- 3
+nac2019$Asisrec[nac2019$Asisrec %in% 6] <- 5
 
 nac2009<-zap_labels(nac2009)
 nac2010<-zap_labels(nac2010)
@@ -217,34 +240,51 @@ nacimientos$Escolap[nacimientos$Escolap %in% "5"] <- "Universitario"
 nacimientos$Escolap[nacimientos$Escolap %in% "6"] <- "Universitario"
 nacimientos$Escolap[nacimientos$Escolap %in% "7"] <- "Universitario"
 
+nacimientos$Onzas[nacimientos$Onzas %in% 99] <- 0
+
+nacimientos$Onzas<-nacimientos$Onzas*0.0625
+nacimientos$Libras<-nacimientos$Libras+nacimientos$Onzas
+nacimientos$Onzas<-NULL
+
 #Evitando overfitting
 nacimientos$Mesreg<-NULL
 nacimientos$Depreg<-NULL
 nacimientos$mupreg<-NULL
 nacimientos$Onzas<-NULL
+
 nacimientos$Mupocu<-NULL
 nacimientos$muprep<-NULL
 nacimientos$Muprem<-NULL
 nacimientos$Mupnap<-NULL
 nacimientos$Mupnam<-NULL
-nacimientos$Escolam<-NULL
+
 nacimientos$Añoreg<-NULL
 
+nacimientos$Tohinm<-NULL
+nacimientos$Tohivi<-NULL
+#nacimientos$Tohite
+
 #Predicciones padre
+
 nacDatP<-nacimientos[!(nacimientos$Escolap==9),]
 
+nacDatP$Escolam<-NULL
+nacDatP$Edadm<-NULL
+nacDatP$Deprem<-NULL
+nacDatP$grupetma<-NULL
+nacDatP$Escivm<-NULL
+nacDatP$Depnam<-NULL
+
+
 nacDatP$Escolap <- as.factor(nacDatP$Escolap)
-nacDatP$Asisrec <- as.factor(nacDatP$Asisrec)
-nacDatP$Sitioocu <- as.factor(nacDatP$Sitioocu)
 nacDatP$Depocu <- as.factor(nacDatP$Depocu)
 nacDatP$Sexo <- as.factor(nacDatP$Sexo)
 nacDatP$Tipar <- as.factor(nacDatP$Tipar)
+nacDatP$Deprep <- as.factor(nacDatP$Deprep)
 nacDatP$Gretnp <- as.factor(nacDatP$Gretnp)
-nacDatP$Escivm <- as.factor(nacDatP$Escivm)
 nacDatP$Escivp <- as.factor(nacDatP$Escivp)
-nacDatP$Depnam <- as.factor(nacDatP$Depnam)
-nacDatP$Depnap <- as.factor(nacDatP$Depnap)
-
+nacDatP$Asisrec <- as.factor(nacDatP$Asisrec)
+nacDatP$Sitioocu <- as.factor(nacDatP$Sitioocu)
 
 
 table(nacDatP$Escolap)
@@ -262,18 +302,18 @@ uniPa<-nacDatP[nacDatP$Escolap=="Universitario",]
 numFilasTrainNoEdPa<-sample(nrow(noEDPa), 0.6*nrow(noEDPa))#6
 trainNoEdPa<-noEDPa[numFilasTrainNoEdPa,]
 
-numFilasTrainPriPa<-sample(nrow(priPa), 0.35*nrow(priPa))#35
+numFilasTrainPriPa<-sample(nrow(priPa), 0.29*nrow(priPa))#35
 trainPriPa<-priPa[numFilasTrainPriPa,]
 
 numFilasTrainBasPa<-sample(nrow(basPa), 0.8*nrow(basPa))#8
 trainBasPa<-basPa[numFilasTrainBasPa,]
 
-numFilasTrainDivPa<-sample(nrow(divPa), 0.8*nrow(divPa))#8
+numFilasTrainDivPa<-sample(nrow(divPa), 0.7*nrow(divPa))#8
 trainDivPa<-divPa[numFilasTrainDivPa,]
 
 numFilasTrainUniPa<-sample(nrow(uniPa), 0.7*nrow(uniPa))#7
 trainUniPa<-uniPa[numFilasTrainUniPa,]
-trainUniPa<-rbind(trainUniPa, trainUniPa[rep(1:60000, 3), ])
+trainUniPa<-rbind(trainUniPa, trainUniPa[rep(1:60000, 2), ])
 
 
 
@@ -308,7 +348,7 @@ table(nacDatP$Escolap)
 arbolModelo<-rpart(Escolap~.,training,method = "class")
 rpart.plot(arbolModelo)
 
-prediccion <- predict(arbolModelo, newdata = test[1:21])
+prediccion <- predict(arbolModelo, newdata = test[1:14])
 columnaMasAlta<-apply(prediccion, 1, function(x) colnames(prediccion)[which.max(x)])
 test$prediccion<-columnaMasAlta
 
@@ -319,3 +359,21 @@ cfm <- confusionMatrix(test$prediccion, test$Escolap)
 cfm
 
 #Bayes Ingenuo Prediccion Educacion del padre
+
+training$Diaocu <- as.factor(training$Diaocu)
+training$Mesocu <- as.factor(training$Mesocu)
+training$Depnap <- as.factor(training$Depnap)
+
+test$Diaocu <- as.factor(test$Diaocu)
+test$Mesocu <- as.factor(test$Mesocu)
+test$Depnap <- as.factor(test$Depnap)
+
+
+modelo<-naiveBayes(as.factor(Escolap)~., data=training)
+
+predTest<-predict(modelo, newdata = test[,1:14])
+test$prediccion <- predTest
+test$prediccion <- as.factor(test$prediccion)
+levels(test$prediccion) <- levels(test$Escolap)
+cfm <- confusionMatrix(test$prediccion, test$Escolap)
+cfm
